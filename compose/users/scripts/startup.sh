a22b17fb507288ffc25279cdeaf3ae5bd28f1985
Raju Gandhi <raju.gandhi@gmail.com>
2020-02-25 18:34:01 -0500

    feat: demo initial commit — blank out files that we are going to fill in

--- compose/users/scripts/startup.sh
+++ compose/users/scripts/startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-./scripts/wait-for.sh \
-  && java -jar /var/app.jar \
-     --spring.datasource.url=jdbc:mysql://db:3306/db_example \
-     --spring.datasource.username="${1}" \
-     --spring.datasource.password="${2}"
+
